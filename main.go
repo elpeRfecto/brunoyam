76e72c0142462b11a4fe2003975f540f802dfbf2
alex <mmjkee@yandex.ru>
2024-11-19 16:49:38 +0300

    first lesson easy level

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	easyLevel := [5]int{10, 2, 3, 4, 5}
+	resEasy := easyHomeWork(easyLevel[:])
+	fmt.Println("Результат простого задания:", resEasy)
+}
+
+func easyHomeWork(arr []int) int {
+	var sum int
+	for i := 0; i < len(arr); i++ {
+		sum += arr[i]
+	}
+	return sum
+}
