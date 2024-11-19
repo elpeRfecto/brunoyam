b4316f0e3045634d1556c0bf7bc405ac6c939f1d
alex <mmjkee@yandex.ru>
2024-11-19 16:55:35 +0300

    first lesson normal level

--- main.go
+++ main.go
@@ -2,18 +2,42 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	easyLevel := [5]int{10, 2, 3, 4, 5}
-	resEasy := easyHomeWork(easyLevel[:])
-	fmt.Println("Результат простого задания:", resEasy)
+	userNameList := UserNameSlice()
+	userInputChar := userInput()
+	resNormal := normalHomework(userInputChar, userNameList)
+	fmt.Println("Результат задания уровня normal:", resNormal)
 }
 
-func easyHomeWork(arr []int) int {
-	var sum int
-	for i := 0; i < len(arr); i++ {
-		sum += arr[i]
+func UserNameSlice() []string {
+	nameSlice := []string{}
+	var point int
+	fmt.Println("Сколько имен вы хотите ввести? ")
+	fmt.Scanln(&point)
+	for i := 0; i < point; i++ {
+		var name string
+		fmt.Scanln(&name)
+		nameSlice = append(nameSlice, name)
 	}
-	return sum
+	return nameSlice
+}
+
+func userInput() string {
+	fmt.Println("С какой буквы ищем имена? ")
+	var input string
+	fmt.Scanln(&input)
+	return strings.TrimSpace(input)
+}
+
+func normalHomework(s string, nameSlice []string) []string {
+	var res []string
+	for _, name := range nameSlice {
+		if strings.HasPrefix(strings.ToLower(name), strings.ToLower(s)) {
+			res = append(res, name)
+		}
+	}
+	return res
 }
