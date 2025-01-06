bc92937d286335ef7ede5ca5f1a17e98aede2001
elpeRfecto <mmjkee@yandex.ru>
2025-01-06 19:13:00 +0300

    added a second solution to the hard case

--- tenth_lesson/hard/hard.go
+++ tenth_lesson/hard/hard.go
@@ -1,5 +1,10 @@
 package main
 
+// import (
+// 	"fmt"
+// 	"sync/atomic"
+// )
+
 import (
     "fmt"
     "sync"
@@ -7,19 +12,36 @@ import (
 )
 
 func main() {
-	//pipe := make(chan int)
-    var tickets uint64
-    var wg sync.WaitGroup
-    for i := 0; i < 10; i++ {
-        wg.Add(1)
-        go func() {
-            for c := 0; c < 10; c++ {
-                atomic.AddUint64(&tickets, 1)
+	var tickets uint64
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+	    wg.Add(1)
+	    go func() {
+	        for c := 0; c < 10; c++ {
+	            atomic.AddUint64(&tickets, 1)
 				fmt.Printf("Билет %d продан\n", atomic.LoadUint64(&tickets))
-            }
-            wg.Done()
-        }()
+	        }
+	        wg.Done()
+	    }()
+	}
+	wg.Wait()
+	fmt.Println("Продано билетов:", tickets)
+
+
+    //ЗДЕСЬ ВТОРОЙ ВАРИАНТ
+
+	// pipe := make(chan int)
+	// var tickets int64
+	// go func() {
+	// 	for i := 0; i < 10; i++ {
+	// 		pipe <- i
+	// 	}
+	// 	close(pipe)
+	// }()
+
+    // for i := 0; i < 100; i++ {
+    //     <-pipe
+    //     atomic.AddInt64(&tickets, 1)
+    //     fmt.Printf("Билет %d продан\n", atomic.LoadInt64(&tickets))
+
     }
-    wg.Wait()
-    fmt.Println("Продано билетов:", tickets)
-}
